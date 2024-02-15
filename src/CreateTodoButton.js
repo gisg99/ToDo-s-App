859af18cebfda4c178aba294b4689ddd163d16ff
Gabriel Sanchez <gabriel.sanchez4294@alumnos.udg.mx>
2024-02-15 16:26:59 -0600

    Optimizando agregar ToDos (Agregar al pulsar enter y no poder agregar strings vacias)

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -4,12 +4,15 @@ import './CreateTodoButton.css';
 function CreateTodoButton(props){
     const handleAddClick = () => {
         const inputS = document.getElementsByClassName("newToDoInput")[0];
-        const newToDo = inputS.value;
-        props.onAdd(newToDo);
+        if(inputS.value != ""){
+            const newToDo = inputS.value;
+            props.onAdd(newToDo);
+            inputS.value = "";
+        }
     }
     
     return(<div className='buttonContainer'>
-        <input className='newToDoInput' placeholder='Crear un nuevo ToDo'/>
+        <input className='newToDoInput' placeholder='Crear un nuevo ToDo' onKeyDown={(e) => {if(e.key === "Enter")handleAddClick()}}/>
         <button onClick={handleAddClick}>+</button>
         </div>
     );
