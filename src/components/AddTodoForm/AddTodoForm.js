d72b38f2b85bce72acf2623bc2a16750ef0ca673
Gabriel Sanchez <gabriel.sanchez@deepia.mx>
2024-09-15 21:30:38 -0600

    Changing structure in addTodo for not saving input text in a variable, saving it in a React State

--- src/components/AddTodoForm/AddTodoForm.js
+++ src/components/AddTodoForm/AddTodoForm.js
@@ -6,16 +6,25 @@ function AddTodoForm() {
         addToDo,
         setModalOpened,
     } = React.useContext(TodoContext);
+    const [todoText, setTodoText] = React.useState("");
     const handleAddClick = () => {
-        let text = document.getElementById("newToDoInput").value;
-        addToDo(text);
+        addToDo(todoText);
         setModalOpened(false);
     }
     
+    const onTodoTextChange = (event) => {
+        setTodoText(event.target.value);
+    }
+
     return(
         <div className="modal-content">
             <h3>¡Crea un nuevo ToDo!</h3>
-            <input id='newToDoInput' placeholder='Subir cambios a GitHub' onKeyDown={(e) => {if(e.key === "Enter") handleAddClick()}}/>
+            <input id='newToDoInput'
+                placeholder='Subir cambios a GitHub'
+                value={todoText}
+                onChange={onTodoTextChange}
+                onKeyDown={(e) => {if(e.key === "Enter") handleAddClick()}}
+            />
             <button onClick={() => handleAddClick()}>Agregar ToDo</button>
         </div>
     );
